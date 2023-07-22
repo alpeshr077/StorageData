b4b7187dbbb1c377d1d5e91f99208a2157332f62
alpeshr077 <alpeshrathava301@gmail.com>
2023-07-22 10:41:16 +0530

    commit

--- app/src/main/java/com/example/storagedata/MainActivity.kt
+++ app/src/main/java/com/example/storagedata/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var Uri: Uri
     lateinit var storage: StorageReference
     lateinit var refDB : DatabaseReference
+    var Image_Code = 12
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding.btnSelect.setOnClickListener {
             var intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
-            startActivityForResult(intent, 12)
+            startActivityForResult(intent, Image_Code)
         }
         binding.btnUpload.setOnClickListener {
             val ref = storage.child("images/${Uri.lastPathSegment}.jpg")
@@ -46,7 +47,7 @@ class MainActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     val downloadUri = task.result
                     var Key = refDB.root.push().key
-                    refDB.root.child("Images").child(Key!!).child("image").setValue(downloadUri)
+                    refDB.root.child("Images").child(Key!!).child("image").setValue(downloadUri.toString())
 
 
                 } else {
@@ -60,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == RESULT_OK) {
-            if (requestCode == 12) {
+            if (requestCode == Image_Code) {
 
 
                 Uri = data?.data!!
